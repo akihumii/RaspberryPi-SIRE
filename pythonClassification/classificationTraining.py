b59e2439336739290e7e5483caa09fd6cb8583e6
akihumii <a0126587@u.nus.edu>
2019-03-22 17:22:56 +0800

    added prof yen's method XD

--- pythonClassification/classificationTraining.py
+++ pythonClassification/classificationTraining.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pickle
 from sklearn.svm import SVC
+from sklearn import preprocessing
 import os
 import sys
 
@@ -19,23 +20,36 @@ def train(target_file):
         file_class = [f for f in os.listdir(target_dir) if f.startswith('classCh')]
 
         for i in range(len(file_class)):
+            # get features and classes from csv files
             features_tmp = np.genfromtxt(os.path.join(target_dir, file_feature[i]), delimiter=',', defaultfmt='%f')
-            
             class_tmp = np.genfromtxt(os.path.join(target_dir, file_class[i]), delimiter=',', defaultfmt='%f')
+
+            # normalize features
+            # features_normalized = features_tmp
+            # [features_normalized, norms] = preprocessing.normalize(features_tmp, norm='max', axis=0, return_norm=True)
+            norms = np.mean(features_tmp, axis=0)
+            features_normalized = features_tmp / norms
+            # norms = preprocessing.MinMaxScaler().fit(features_tmp)
+            # features_normalized = norms.transform(features_tmp)
 
             print('fitting %s' % file_feature[i])
 
-            gamma_value = 1./(np.size(features_tmp, 1)*features_tmp.std())
+            # training SVC
+            gamma_value = 1./(np.size(features_normalized, 1)*features_normalized.std())
+            clf = SVC(kernel='poly', C=50.0, gamma=gamma_value)
+            classifiers = clf.fit(features_normalized, class_tmp)
 
-            clf = SVC(kernel='poly', gamma=gamma_value)
-
-            classifiers = clf.fit(features_tmp, class_tmp)
-
+            # saving classifiers and norms
             filename = 'classifierCh%s.sav' % file_feature[i][file_feature[i].find('Ch')+2]
+            filename_norms = 'normsCh%s.csv' % file_feature[i][file_feature[i].find('Ch') + 2]
+            # filename_norms = 'normsCh%s.sav' % file_feature[i][file_feature[i].find('Ch')+2]
 
             pickle.dump(classifiers, open(os.path.join(target_dir, filename), 'wb'))
-            print("%s%d has been saved..." % (os.path.join(target_dir, filename), i))
+            np.savetxt(os.path.join(target_dir, filename_norms), norms, delimiter=',')
+            # pickle.dump(norms, open(os.path.join(target_dir, filename_norms), 'wb'))
 
+            print("%s%d has been saved..." % (os.path.join(target_dir, filename), i))
+            print("%s%d has been saved..." % (os.path.join(target_dir, filename_norms), i))
     else:
         print("no %s is found..." % target_dir)
 
@@ -43,5 +57,6 @@ def train(target_file):
 
 
 if __name__ == "__main__":
-    # train('C:\\Users\\lsitsai\\Desktop\\Marshal\\20190131_Chronic_NHP_wireless_implant_Alvin\\Info\\classificationTmp')
-    train(str(sys.argv[1]))
+    train('C:\\Users\\lsitsai\\Desktop\\Marshal\\20190131_Chronic_NHP_wireless_implant_Alvin\\Info\\classificationTmp')
+    # train('C:\\Users\\lsitsai\\Desktop\\Marshal\\20190131_Chronic_NHP_wireless_implant_Alvin\\Info\\classificationTmp\\storage\\normalized')
+    # train(str(sys.argv[1]))
--- pythonClassification/visualizeClf.py
+++ pythonClassification/visualizeClf.py
@@ -4,10 +4,13 @@ import pickle
 import os
 
 target_dir = 'C:\\Users\\lsitsai\\Desktop\\Marshal\\20190131_Chronic_NHP_wireless_implant_Alvin\\Info\\classificationTmp'
+# target_dir = 'C:\\Users\\lsitsai\\Desktop\\Marshal\\20190131_Chronic_NHP_wireless_implant_Alvin\\Info\\classificationTmp\\storage\\normalized'
 
 file_feature = [f for f in os.listdir(target_dir) if f.startswith('featuresCh')]
 
 file_class = [f for f in os.listdir(target_dir) if f.startswith('classCh')]
+
+file_norms = [f for f in os.listdir(target_dir) if f.startswith('normsCh')]
 
 file_clf = [f for f in os.listdir(target_dir) if f.startswith('classifierCh')]
 
@@ -21,19 +24,28 @@ for i in range(len(file_feature)):
     # load classes
     classes = np.genfromtxt(os.path.join(target_dir, file_class[i]), delimiter=',', defaultfmt='%f')
 
+    # load norms
+    norms = np.genfromtxt(os.path.join(target_dir, file_norms[i]), delimiter=',')
+    # norms = pickle.load(open(os.path.join(target_dir, file_norms[i]), 'rb'))
+
+    # normalize the features
+    # features_normalized = features
+    features_normalized = features / norms
+    # features_normalized = norms.transform(features)
+
     plt.figure(i)
     plt.clf()
 
     plt.scatter(clf.support_vectors_[:, 0], clf.support_vectors_[:, 1], s=80,
                 facecolors='none', zorder=10, edgecolors='k')
-    plt.scatter(features[:, 0], features[:, 1], c=classes, zorder=10, cmap=plt.cm.Paired,
+    plt.scatter(features_normalized[:, 0], features_normalized[:, 1], c=classes, zorder=10, cmap=plt.cm.Paired,
                 edgecolors='k')
 
     plt.axis('tight')
-    x_min = min(features[:, 0])
-    x_max = max(features[:, 0])
-    y_min = min(features[:, 1])
-    y_max = max(features[:, 1])
+    x_min = min(features_normalized[:, 0])
+    x_max = max(features_normalized[:, 0])
+    y_min = min(features_normalized[:, 1])
+    y_max = max(features_normalized[:, 1])
 
     XX, YY = np.mgrid[x_min:x_max:200j, y_min:y_max:200j]
     Z = clf.decision_function(np.c_[XX.ravel(), YY.ravel()])
@@ -47,12 +59,9 @@ for i in range(len(file_feature)):
     plt.xlim(x_min, x_max)
     plt.ylim(y_min, y_max)
 
-    plt.xticks(())
-    plt.yticks(())
-
     # axes labels
     label_x = 'meanValue'
-    label_y = 'numZeroCrossings'
+    label_y = 'numSignChanges'
     label_title = file_class[i]
 
     plt.xlabel(label_x)
