fd047fbc19e2d35215444168f272db14d970f8d0
akihumii <a0126587@u.nus.edu>
2019-03-28 16:31:09 +0800

    added in delay during the initialization, added the conversion of pulse duration :)

--- pythonClassification/command_odin.py
+++ pythonClassification/command_odin.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 
 
 class CommandOdin:
@@ -22,7 +23,7 @@ class CommandOdin:
         self.command_start = [0xF8, 0xF8]
         self.command_stop = [0x8F, 0x8F]
 
-        self.amplitude = np.zeros(4, dtype=np.double)
+        self.amplitude = 1 * np.ones(4, dtype=np.double)
         self.channel_enable = 0
         self.pulse_duration = 200 * np.ones(4, dtype=int)
         self.frequency = 50
@@ -36,10 +37,15 @@ class CommandOdin:
         self.sock = socket
 
     def send_initialise(self):  # send all parameters except channel enable
-        self.send_frequency()
-        [self.send_pulse_duration(i) for i in range(self.num_channel)]
         self.get_coefficients()
-        [self.send_amplitude(self._get_amplitude_byte(i)) for i in range(self.num_channel)]
+        time.sleep(0.8)
+        self.send_frequency()
+        for i in range(self.num_channel):
+            time.sleep(1 + i*0.2)
+            self.send_pulse_duration(self._get_pulse_duration_byte(i))
+        for i in range(self.num_channel):
+            time.sleep(1.8 + i*0.2)
+            self.send_amplitude(self._get_amplitude_byte(i))
 
     def send_start(self):
         self.sock.send(self.command_start)
@@ -68,15 +74,18 @@ class CommandOdin:
         self.sock.send([address, self.channel_enable])
 
     def get_coefficients(self):
-        coefficients = np.genfromtxt('formula.csv', delimiter=',', defaultfmt='%f')
+        coefficients = np.genfromtxt('formula.txt', delimiter=',', defaultfmt='%f')
         self.amplitude_a = coefficients[0]
         self.amplitude_b = coefficients[1]
         self.amplitude_c = coefficients[2]
 
+    def _get_pulse_duration_byte(self, channel):
+        output = int(np.array(self.pulse_duration[channel]/5 - 3))
+        return output
+
     def _get_amplitude_byte(self, channel):
         output = self.amplitude[channel]**2*self.amplitude_a + self.amplitude[channel]*self.amplitude_b - self.amplitude_c  # the conversion into bytes
         output = int(output)
-
         return output
 
 
