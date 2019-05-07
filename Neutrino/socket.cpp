70a285bd3053e6fcf1f07c28e6ad5ecd380e3cde
Le Nguyen <surln@nus.edu.sg>
2019-05-07 10:35:07 +0800

    Update socket.cpp

--- Neutrino/socket.cpp
+++ Neutrino/socket.cpp
@@ -19,7 +19,8 @@
 #include "../common/ringbuffer.h"
 #include "../common/parallel-gpio.h"
 
-#define BUFFER_SIZE 2040
+#define BUFFER_SIZE 160
+#define BUFFER_SIZE_2 3200
 #define SOCKET_SERVER_PORT 8888
 
 INITIALIZE_EASYLOGGINGPP
@@ -31,18 +32,21 @@ struct sockaddr_in server, client, odin_addr;
 
 unsigned char parallelbuffer[BUFFER_SIZE];
 unsigned char readbuffer[BUFFER_SIZE];
-unsigned char writebuffer[BUFFER_SIZE];
+unsigned char writebuffer[BUFFER_SIZE_2];
 unsigned char recvbuffer[32];
 unsigned char sync_buf[32];
 
 bool read_bool = 0;
 
 int recv_count = 0;
+
 bool isFrDetected = false;
 uint8_t counter = 0;
-int countIndex = 0;
-int errCount = 0;
-int errCountMain = 0;
+int counterIndex = 0;
+
+bool isFrDetected2 = false;
+uint8_t counter2 = 0;
+int counterIndex2 = 0;
 
 
 WiringPiSerial *fd;
@@ -57,7 +61,7 @@ pthread_mutex_t lock1, lock2;
 BITMODE selectedMode = BITMODE_8;
 
 static int findSyncPulse(unsigned char* buf, int len){
-    int i;
+    int i = 0;
     if(data_stream->getBitMode() == BITMODE_8){
         for(i = 0; i < len-4; i++){
             if((uint8_t) buf[i] == 0xF0 && (uint8_t) buf[i+4] == 0){
@@ -77,11 +81,12 @@ static int findSyncPulse(unsigned char* buf, int len){
 
 static void acceptConnection(void){
     client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t *)&c);
-    fcntl(client_sock, F_SETFL, O_NONBLOCK);
+    // fcntl(client_sock, F_SETFL, O_NONBLOCK);
     setsockopt(client_sock, IPPROTO_TCP, TCP_NODELAY, (void *)&flag, sizeof(flag));
 
     if(client_sock<0){
         LOG(ERROR) << ", \"event\":\"Accepting:\", \"errnum\":0, \"msg\":\"Socket Connection Error!\"";
+        close(socket_desc);
     }
     LOG(INFO) << ", \"event\":\"Accepting:\", \"errnum\":0, \"msg\":\"Socket connected!\"";
 }
@@ -106,7 +111,7 @@ static void connectOdin(void){
     }while(temp < 0);
     fprintf(stdout, "Connected to Odin\n");
 
-    fcntl(odin_socket, F_SETFL, O_NONBLOCK);
+    // fcntl(odin_socket, F_SETFL, O_NONBLOCK);
     setsockopt(odin_socket, IPPROTO_TCP, TCP_NODELAY, (void *)&flag, sizeof(flag));
 }
 
@@ -150,35 +155,74 @@ void initParallelTask(){
 
 void verifyCounter(){
     if (isFrDetected == false){
-        countIndex = findSyncPulse(parallelbuffer, BUFFER_SIZE) -1;
-        if (countIndex != -1){
+        if (data_stream->getBitMode() == BITMODE_8){
+            counterIndex = findSyncPulse(parallelbuffer, BUFFER_SIZE) - 3;
+        }
+        else{
+            counterIndex = findSyncPulse(parallelbuffer, BUFFER_SIZE) - 7;
+        }
+
+        if (counterIndex != -1){
+            counter = parallelbuffer[counterIndex];
             isFrDetected = true;
-            counter = parallelbuffer[countIndex];
-            printf("new count: %d %d", counter, parallelbuffer[countIndex]);
         }
     }
 
-    while(countIndex < BUFFER_SIZE && isFrDetected == true){
-        if (counter != parallelbuffer[countIndex]){
-            if (errCount<5){
-                printf("err: %d %d", counter, parallelbuffer[countIndex]);
-                counter = parallelbuffer[countIndex];
-            }
-            else {
-                isFrDetected = false;
-                errCount = 0;
-                errCountMain++;
-                break;
-            }
-            errCount++;
+    while(counterIndex < BUFFER_SIZE){
+        if (counter != parallelbuffer[counterIndex]){
+            printf("err counter index read: %d %d\n", counter, parallelbuffer[counterIndex]);
+            isFrDetected = false;
+            // break;
         }
-        counter++;
-        countIndex += 17;
-    }
 
-    countIndex -= BUFFER_SIZE;
+        if (data_stream->getBitMode() == BITMODE_8){
+            counter = (counter + 2) % 256;
+            counterIndex += 16;
+        }
+        else{
+            counter = (counter + 2) % 32;
+            counterIndex += 32;
+        }
+    }
+    counterIndex = counterIndex % BUFFER_SIZE;
+
+    if (!isFrDetected){
+        for (int i = 0; i < BUFFER_SIZE; i ++){
+            printf("%d ", parallelbuffer[i]);
+        }
+        printf("\n ");
+    }
 
 }
+
+void verifyCounter2(){
+    counterIndex2 = findSyncPulse(writebuffer, BUFFER_SIZE_2) - 3;
+
+    if (!isFrDetected2 && counterIndex2 != -1){
+        counter2 = writebuffer[counterIndex2];
+        isFrDetected2 = true;
+    }
+
+    while(counterIndex2 < BUFFER_SIZE_2 && isFrDetected2 == true){
+        if (counter2 != writebuffer[counterIndex2]){
+            printf("err count index wifi: %d %d\n", counter2, writebuffer[counterIndex2]);
+            isFrDetected2 = false;
+            break;
+        }
+        counter2 = (counter2 + 2) % 256;
+        counterIndex2 += 16;
+    }
+    counterIndex2 -= BUFFER_SIZE_2;
+
+    if (!isFrDetected2){
+        for (int i = 0; i < BUFFER_SIZE_2; i ++){
+            printf("%d ", writebuffer[i]);
+        }
+        printf("\n ");
+    }
+
+}
+
 
 void setupCmdSettings(){
     switch(recvbuffer[6]){
@@ -243,12 +287,16 @@ void resolveCmd(){
 void storeData(){
     data_stream->SafeRead(parallelbuffer, BUFFER_SIZE);
 
-    if (errCountMain<5){
-        verifyCounter();
-    }
+    // if (data_stream->getBitMode() == BITMODE_5){
+    //     return;
+    // }
 
-    rb_parallel->write(parallelbuffer, BUFFER_SIZE);
+    // verifyCounter();
+
+    pthread_mutex_lock(&lock1);
     rb->write(parallelbuffer, BUFFER_SIZE);
+    // rb_parallel->write(parallelbuffer, BUFFER_SIZE);
+    pthread_mutex_unlock(&lock1);
     memset(parallelbuffer, '\0', BUFFER_SIZE);
 }
 
@@ -265,12 +313,16 @@ void slotSyncPulse(){
 void *readParallel(void *arg){
     initParallelTask();
     while(1){
-        if(client_sock > 0){
-            resolveCmd();
+        if(rb->getFree() < BUFFER_SIZE+1){
+            printf("buffer overflow, write\n");
+            while (rb->getFree() < BUFFER_SIZE+1){}
         }
-        if(rb_parallel->getFree() > BUFFER_SIZE+1 && read_bool){
-            storeData();
+        if(read_bool){
+        pthread_mutex_lock(&lock2);
+        storeData();
+        pthread_mutex_unlock(&lock2);
         }
+
     }
 }
 
@@ -294,14 +346,32 @@ void *readPacket(void *arg){
 
 void *sendWiFi(void *arg){
     while(1){
-        if(rb->getOccupied() > BUFFER_SIZE+1){
-            rb->read(writebuffer, BUFFER_SIZE);
-            if(client_sock > 0){
-                if(write(client_sock, writebuffer, BUFFER_SIZE) < 0){
+        if(rb->getOccupied() > BUFFER_SIZE_2+1){
+            pthread_mutex_lock(&lock1);
+            int len = rb->read(writebuffer, BUFFER_SIZE_2);
+            if (len != BUFFER_SIZE_2){
+                printf("buffer overflow, read\n");
+            }
+            pthread_mutex_unlock(&lock1);
 
+            // verifyCounter2();
+
+            if(client_sock > 0){
+                if(write(client_sock, writebuffer, BUFFER_SIZE_2) < 0){
+                    printf("wifi packet dropped.\n");
                 }
             }
-            memset(writebuffer, '\0', BUFFER_SIZE);
+            memset(writebuffer, '\0', BUFFER_SIZE_2);
+        }
+    }
+}
+
+void *recvWiFi(void *arg){
+    while(1){
+        if(client_sock > 0){
+            pthread_mutex_lock(&lock2);
+            resolveCmd();
+            pthread_mutex_unlock(&lock2);
         }
     }
 }
@@ -313,46 +383,52 @@ int main(int argc, char *argv[]){
     }
     LOG(INFO) << ", \"event\":\"wiringpi-Setup\", \"errnum\":0, \"msg\":\"Wiring Pi Setup Successful!\"";
 
-    rb = new ringbuffer<uint8_t>(BUFFER_SIZE * 20);
-    if(rb->getFree() < 0){
-        LOG(ERROR) << ", \"event\":\"Ring buffer initialisation\", \"errnum\":0, \"msg\":\"Ring buffer could not be created!\"";
-    }
-    LOG(INFO) << ", \"event\":\"Ring buffer initialisation\", \"errnum\":0, \"msg\":\"Ring buffer created!\"";
+    rb = new ringbuffer<uint8_t>(BUFFER_SIZE_2 * 1000);
+    rb_parallel = new ringbuffer<uint8_t>(BUFFER_SIZE * 10);
 
-    rb_parallel = new ringbuffer<uint8_t>(BUFFER_SIZE * 20);
-    if(rb_parallel->getFree() < 0){
-        LOG(ERROR) << ", \"event\":\"Ring buffer initialisation\", \"errnum\":0, \"msg\":\"Ring buffer could not be created!\"";
-    }
-    LOG(INFO) << ", \"event\":\"Ring buffer initialisation\", \"errnum\":0, \"msg\":\"Ring buffer created!\"";
+    // if(rb->getFree() < 0){
+    //     LOG(ERROR) << ", \"event\":\"Ring buffer initialisation\", \"errnum\":0, \"msg\":\"Ring buffer could not be created!\"";
+    // }
+    // LOG(INFO) << ", \"event\":\"Ring buffer initialisation\", \"errnum\":0, \"msg\":\"Ring buffer created!\"";
 
-    if (pthread_mutex_init(&lock1, NULL) != 0)
-    {
-        LOG(ERROR) << ", \"event\":\"Mutex Initialisation\", \"errnum\":0, \"msg\":\"Mutex Initialisation Failed!\"";
-        return -1;
-    }
-    LOG(INFO) << ", \"event\":\"Mutex Initialisation\", \"errnum\":0, \"msg\":\"Mutex Initiation Successful!\"";
+    // if(rb_parallel->getFree() < 0){
+    //     LOG(ERROR) << ", \"event\":\"Ring buffer initialisation\", \"errnum\":0, \"msg\":\"Ring buffer could not be created!\"";
+    // }
+    // LOG(INFO) << ", \"event\":\"Ring buffer initialisation\", \"errnum\":0, \"msg\":\"Ring buffer created!\"";
 
-    if (pthread_mutex_init(&lock2, NULL) != 0)
-    {
-        LOG(ERROR) << ", \"event\":\"Mutex Initialisation\", \"errnum\":0, \"msg\":\"Mutex Initialisation Failed!\"";
-        return -1;
-    }
-    LOG(INFO) << ", \"event\":\"Mutex Initialisation\", \"errnum\":0, \"msg\":\"Mutex Initiation Successful!\"";
+    // if (pthread_mutex_init(&lock1, NULL) != 0)
+    // {
+    //     LOG(ERROR) << ", \"event\":\"Mutex Initialisation\", \"errnum\":0, \"msg\":\"Mutex Initialisation Failed!\"";
+    //     return -1;
+    // }
+    // LOG(INFO) << ", \"event\":\"Mutex Initialisation\", \"errnum\":0, \"msg\":\"Mutex Initiation Successful!\"";
 
-    connectOdin();
+    // if (pthread_mutex_init(&lock2, NULL) != 0)
+    // {
+    //     LOG(ERROR) << ", \"event\":\"Mutex Initialisation\", \"errnum\":0, \"msg\":\"Mutex Initialisation Failed!\"";
+    //     return -1;
+    // }
+    // LOG(INFO) << ", \"event\":\"Mutex Initialisation\", \"errnum\":0, \"msg\":\"Mutex Initiation Successful!\"";
+
+    pthread_mutex_init(&lock1, NULL);
+    pthread_mutex_init(&lock2, NULL);
+
+    // connectOdin();
     waitForSylph();
 
-    if(pthread_create(&(tid[0]), NULL, &readPacket, NULL) < 0){
-        LOG(ERROR) << ", \"event\":\"Creating readPacket Thread:\", \"errnum\":0, \"msg\":\"Packet Thread could not be created!\"";
-    }
-    LOG(INFO) << ", \"event\":\"Creating readPacket Thread:\", \"errnum\":0, \"msg\":\"Packet Thread created!\"";
+    // if(pthread_create(&(tid[0]), NULL, &readPacket, NULL) < 0){
+    //     LOG(ERROR) << ", \"event\":\"Creating readPacket Thread:\", \"errnum\":0, \"msg\":\"Packet Thread could not be created!\"";
+    // }
+    // LOG(INFO) << ", \"event\":\"Creating readPacket Thread:\", \"errnum\":0, \"msg\":\"Packet Thread created!\"";
 
-    if(pthread_create(&(tid[1]), NULL, &sendWiFi, NULL) < 0){
+    if(pthread_create(&(tid[2]), NULL, &sendWiFi, NULL) < 0){
         LOG(ERROR) << ", \"event\":\"Creating WiFi Thread:\", \"errnum\":0, \"msg\":\"WiFi Thread could not be created!\"";
     }
     LOG(INFO) << ", \"event\":\"Creating WiFi Thread:\", \"errnum\":0, \"msg\":\"WiFi Thread created!\"";
+    
+    pthread_create(&(tid[2]), NULL, &recvWiFi, NULL);
 
-    if(pthread_create(&(tid[2]), NULL, &readParallel, NULL) < 0){
+    if(pthread_create(&(tid[1]), NULL, &readParallel, NULL) < 0){
         LOG(ERROR) << ", \"event\":\"Creating Parallel Thread:\", \"errnum\":0, \"msg\":\"Parallel Thread could not be created!\"";
     }
     LOG(INFO) << ", \"event\":\"Creating Parallel Thread:\", \"errnum\":0, \"msg\":\"Parallel Thread created!\"";
