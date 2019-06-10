323716e09c14a0a8379adccbb3e0a38a10669537
akihumii <a0126587@u.nus.edu>
2019-06-10 19:48:42 +0800

    receive command from catGUI :)

--- pythonClassification/bypass_data.py
+++ pythonClassification/bypass_data.py
@@ -4,14 +4,24 @@ import copy
 
 
 class BypassData(multiprocessing.Process):
-    def __init__(self, tcp_ip_obj, raw_buffer_event, raw_buffer_queue, change_parameter_queue, change_parameter_event, stop_event):
+    def __init__(self, tcp_ip_obj, raw_buffer_event, raw_buffer_queue, change_parameter_queue, change_parameter_event, filter_parameters_queue, stop_event):
         multiprocessing.Process.__init__(self)
         self.tcp_ip_obj = tcp_ip_obj
         self.raw_buffer_event = raw_buffer_event
         self.raw_buffer_queue = raw_buffer_queue
+        self.filter_parameters_queue = filter_parameters_queue
         self.change_parameter_queue = change_parameter_queue
         self.change_parameter_event = change_parameter_event
         self.stop_event = stop_event
+
+        self.address = {
+            0xD6: True,  # sampling freq
+            0xD7: True,  # highpass cutoff freq
+            0xD8: True,  # lowpass cutoff freq
+            0xD9: True  # notch cutoff freq
+        }
+
+        self.address_sampling_frequency = 0xD6
 
     def run(self):
         print('started bypassing thread...')
@@ -38,7 +48,14 @@ class BypassData(multiprocessing.Process):
                     if len(data_recv) > 0:
                         if len(data_recv) == 1:
                             data_recv = np.append(data_recv, 0)
-                        self.change_parameter_queue.put(data_recv.astype(int))
+                        elif len(data_recv) > 2:
+                            data_recv = [data_recv[0], self._char2int(data_recv[1:])]
+                        if self.address.get(data_recv[0]):
+                            self.filter_parameters_queue.put(data_recv.astype(int))  # put for changing filter object
+                        else:
+                            self.change_parameter_queue.put(data_recv.astype(int))  # put for changing classification parameters
+                        if data_recv[0] == self.address_sampling_frequency:  # put sampling frequency to classification obj as well
+                            self.change_parameter_queue.put(data_recv.astype(int))
                         self.change_parameter_event.set()  # flag to notify there's a change of parameters is needed
 
                     if self.stop_event.is_set():
@@ -55,6 +72,12 @@ class BypassData(multiprocessing.Process):
                 break
 
         print('data bypassing thread has stopped...')
+
+    def _char2int(self, data):
+        output = [chr(int(x)) for x in data]
+        output = ''.join(output)
+        output = np.frombuffer(output, dtype=np.uint16)
+        return int(output)
 
 
 
