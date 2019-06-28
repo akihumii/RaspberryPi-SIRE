cad4ffb13bd14f7e6dba990509976472f3c0a776
akihumii <a0126587@u.nus.edu>
2019-06-28 17:58:03 +0800

    make saving.py can accept a specified filename :D

--- pythonClassification/saving.py
+++ pythonClassification/saving.py
@@ -4,18 +4,25 @@ import numpy as np
 
 
 class Saving:  # save the data
-    def __init__(self):
-        now = datetime.datetime.now()
-        self.__saving_dir_Date = "%d%02d%02d" % (now.year, now.month, now.day)
-        self.__saving_filename = "data%s%02d%02d%02d%02d" % (
-            self.__saving_dir_Date, now.hour, now.minute, now.second, now.microsecond)
-        self.__saving_full_filename = os.path.join("Data", self.__saving_filename) + ".csv"
+    def __init__(self, saving_filename=''):
+        if saving_filename:
+            self.__saving_full_filename = saving_filename
+        else:
+            self.__saving_full_filename = os.path.join("Data", self.get_filename()) + ".csv"
 
         self.__create_saving_dir()  # create saving directory, skip if the file exists
 
     def __create_saving_dir(self):
         if not os.path.exists("Data"):
             os.makedirs("Data")
+
+    def get_filename(self):
+        now = datetime.datetime.now()
+        saving_dir_date = "%d%02d%02d" % (now.year, now.month, now.day)
+        saving_filename = "data%s%02d%02d%02d%02d" % (
+            saving_dir_date, now.hour, now.minute, now.second, now.microsecond)
+
+        return saving_filename
 
     def save(self, data, *args):  # save the data
         saving_file_obj = open(self.__saving_full_filename, *args)
